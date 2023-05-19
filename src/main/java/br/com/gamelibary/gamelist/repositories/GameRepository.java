afffc921c25fa96eb94e78bf09c66a66b9a626d0
Gustavo <liliagustavo@hotmail.com>
2023-05-19 11:46:07 -0300

    Move games on list

--- src/main/java/br/com/gamelibary/gamelist/repositories/GameRepository.java
+++ src/main/java/br/com/gamelibary/gamelist/repositories/GameRepository.java
@@ -3,6 +3,7 @@ package br.com.gamelibary.gamelist.repositories;
 import br.com.gamelibary.gamelist.entities.Game;
 import br.com.gamelibary.gamelist.projection.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -17,4 +18,8 @@ public interface GameRepository extends JpaRepository<Game,Long> {
 		ORDER BY tb_belonging.position
 			""")
 	List<GameMinProjection> searchByList(Long listId);
+
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- src/main/java/br/com/gamelibary/gamelist/services/GameListService.java
+++ src/main/java/br/com/gamelibary/gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package br.com.gamelibary.gamelist.services;
 
 import br.com.gamelibary.gamelist.dto.GameListDTO;
 import br.com.gamelibary.gamelist.entities.GameList;
+import br.com.gamelibary.gamelist.projection.GameMinProjection;
 import br.com.gamelibary.gamelist.repositories.GameListRepository;
+import br.com.gamelibary.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection remove = list.remove(sourceIndex);
+        list.add(destinationIndex,remove);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i<=max;i++){
+            gameListRepository.updateBelongingPosition(listId,list.get(i).getId(),i);
+        }
     }
 
 
